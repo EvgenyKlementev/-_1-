93b04d2326ac0471126897d1c6af2fbfa6dc4f3b
unknown <klementyev.ev@gmail.com>
2023-02-06 01:06:35 +0300

    задача 8

--- Задача_8/Program.cs
+++ Задача_8/Program.cs
@@ -14,11 +14,13 @@ int N = int.Parse(Console.ReadLine());
 int i = 1;
 while(i<=N)
 {
-    if(i %2==0)
+    if(i % 2==0)
     {
       Console.Write($"{i}");
-      i++;
+      
     }
+    
+    i++;
     
 }
 
